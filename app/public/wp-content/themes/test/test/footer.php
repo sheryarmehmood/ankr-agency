f835805a897df57e6ef58cbcce4a6d82fdcd2722
Dev <sheryar.diligent@gmail.com>
2024-02-16 12:16:45 -0800

    footer added

--- app/public/wp-content/themes/test/test/footer.php
+++ app/public/wp-content/themes/test/test/footer.php
@@ -11,7 +11,42 @@
 
 ?>
 
-	<footer id="colophon" class="site-footer">
+	<!-- <footer id="colophon" class="site-footer" style="background-color:#30303b"> -->
+	<footer class="footer" style="background-color:#30303b">
+	<div class="container">
+                <div class="inner">
+                    <div class="footer__top">
+                        <div class="footer__links">
+                            <a href="">Home</a>
+                            <a href="">About Us</a>
+                            <a href="">Marketing Services</a>
+                            <a href="">Contact Us</a>
+                            <a href="">Schedule a Strategy Session</a>
+                        </div>
+                        <div class="footer__social">
+                            <a href="" class="footer__site--logo">
+                                <img src="https://ankragency.com/wp-content/themes/urban_insight/assets/images/logo-white.png"
+                                    alt="">
+                            </a>
+                            <h6>Follow us</h6>
+                            <div class="footer__social--links">
+                                <a href="https://www.facebook.com/ankragency/" class="fa-brands fa-square-facebook"></a>
+                                <a href="https://www.youtube.com/channel/UCWIU4WpYkcc1Sd3J-SLOi-Q"
+                                    class="fa-brands fa-youtube"></a>
+                                <a href="https://twitter.com/ankragency" class="fa-brands fa-twitter"></a>
+                                <a href="https://www.instagram.com/ankragency/?hl=en"
+                                    class="fa-brands fa-instagram"></a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="footer__bottom">
+                        <div class="copyright__text">
+                            <p></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
 		<div class="site-info">
 			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'test' ) ); ?>">
 				<?php
